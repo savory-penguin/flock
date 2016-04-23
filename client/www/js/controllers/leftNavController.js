dbfb89ed138b5b303c1de2cbe103e097252c24d9
michelleheh <xiaoyu.he22@gmail.com>
2016-04-23 13:49:12 -0700

    Update front end code to map markers based on date

--- client/www/js/controllers/leftNavController.js
+++ client/www/js/controllers/leftNavController.js
@@ -1,27 +1,26 @@
 angular.module('amblr.leftnav', ['ionic', 'ionic-datepicker'])
-.controller('LeftMenuNav', function($scope, $ionicPopover, $ionicSideMenuDelegate, ionicDatePicker, Videos) {
+.controller('LeftMenuNav', function($scope, $ionicPopover, $ionicSideMenuDelegate, ionicDatePicker, Videos, sharedProperties, $rootScope) {
   // show signin menu
   $scope.showMenu = function () {
     $ionicSideMenuDelegate.toggleLeft();
   };
-  // default date to today when app opens
-  var today = new Date();
+  // default date to today in services.js
+  var today = sharedProperties.getDate();
   today = (today.getMonth() + 1) + '/' + today.getDate() + '/' +  today.getFullYear();
   $scope.pickedDate = today;
   /* pull up datepicker calendar
   see the github page for the date picker used here
   https://github.com/rajeshwarpatlolla/ionic-datepicker*/
   var ipObj1 = {
+
     callback: function (val) {  //Mandatory
       var date = new Date(val);
+      sharedProperties.setDate(date);
       date = (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear();
       $scope.pickedDate = date;
-      console.log(val);
-      // fectch video with set date
-      Videos.getVideos((new Date(val)).getTime()).then(function(allVids) {
-        console.log('here are all your vids: ', allVids);
-      });
+      $rootScope.$broadcast('newDatePicked');
     },
+
     from: new Date(2012, 1, 1), //Optional
     to: new Date(),             //Optional
     inputDate: new Date(),      //Optional
